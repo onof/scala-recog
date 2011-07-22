73b2f7b37bfc94b1836d7d3335bbee71fb0a6de4
onofrio.panzarino@gmail.com <onofrio.panzarino@gmail.com@7e59156b-7d4f-7ba9-4bbf-c8966603b9ef>
2011-07-22 12:02:06 +0000

    not tested version of ID3

--- src/main/scala/org/scalarecog/decisiontree/ID3.scala
+++ src/main/scala/org/scalarecog/decisiontree/ID3.scala
@@ -13,29 +13,39 @@ class ID3[Property, Label] {
 
    private val entropyCalc = new Entropy[(Data, Label), Label](g => g._2)
 
-   private def indexWhereSplit(properties : Int, dataset : List[(Data, Label)]) = {
-     val entropies = for(i <- 0 to properties; // for each property
-                         entropy = (
-                                    // for each distinct property
-                                    for(feature <- (for ((data, label) <- dataset) yield data(i)).toSet;
-                                        // split the dataset and compute the entropy
-                                        subDataSet = for ((data, label) <- dataset if data(i) == feature) yield (data, label);
-                                        prob = subDataSet.length / dataset.length.toDouble
-                                        ) yield prob * entropyCalc(subDataSet)
-                                    ).sum // and sum the entropy of all of them
-                          ) yield (entropy, i)
+   private def indexWhereSplit(properties : Set[Int], dataset : List[(Data, Label)]) = {
+     val entropies = for(i <- properties; // for each property
+                         entropy = (for((pv, subDataSet) <- splitOnProperty(dataset, i))
+                                    yield (subDataSet.length / dataset.length.toDouble) * entropyCalc(subDataSet)
+                                   ).sum // and sum the entropy of all of them
+                     ) yield (entropy, i)
 
-     // Take the property with the highest entropy
-     entropies.reduceLeft((e1, e2) => if(e1._1 > e2._1) e1 else e2)._2
+     // Take the property with the highest entropy gain
+     entropies.reduceLeft((e1, e2) => if(e1._1 < e2._1) e1 else e2)._2
    }
+
+   private def splitOnProperty(dataset : List[(Data, Label)], propertyId : Int) : Map[Property, List[(Data, Label)]] =
+     (
+       for(
+        propertyValue : Property <- (for ((data, label) <- dataset) yield data(propertyId)).toSet;
+        subDataSet = for ((data, label) <- dataset if data(propertyId) == propertyValue) yield (data, label)
+       ) yield (propertyValue, subDataSet)
+     ) toMap
 
    def buildTree(dataset : List[(Data, Label)]) : DecisionTree[Data, Label] = {
 
-     val features = dataset.head._1.length
+     def buildTreeRecursive(subset : List[(Data, Label)], properties : Set[Int]) : DecisionTree[Data,Label] =
+       subset.map(_._2).toSet.toList match {
+         case List(oneLabel) => new DecisionLeaf[Data,Label](oneLabel)
 
+         case _ => {
+                      val index = indexWhereSplit(properties, subset)
+                      new DecisionBranch[Data,Label,Property](d => d(index),
+                        splitOnProperty(subset, index).mapValues(ds => buildTreeRecursive(ds, properties - index)))
+                   }
+       }
 
-     // TODO
-     null
+     val features = (0 to dataset.head._1.length).toSet
+     buildTreeRecursive(dataset, features)
    }
-
 }
